4902f244adcbb68dc880fbae57f1de8265c6f603
Al Gibran <162325903+Vedahsla@users.noreply.github.com>
2024-06-13 13:06:51 +0700

    Update firebase.js

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,23 +1,25 @@
-const firebase = require('firebase/app');
 const admin = require('firebase-admin');
-require('firebase/auth');
-require('firebase/firestore');
 require('dotenv').config();
 
 const serviceAccount = {
-	type: 'service_account',
-	project_id: process.env.FIREBASE_PROJECT_ID,
-	private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-	client_email: process.env.FIREBASE_CLIENT_EMAIL,
-	client_id: process.env.FIREBASE_CLIENT_ID,
-	auth_uri: 'https://accounts.google.com/o/oauth2/auth',
-	token_uri: 'https://oauth2.googleapis.com/token',
-	auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
-	client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
-  };
+  type: 'service_account',
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key: (process.env.FIREBASE_PRIVATE_KEY || '').replace(/\\n/g, '\n'),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_url: 'https://accounts.google.com/o/oauth2/auth',
+  token_url: 'https://oauth2.googleapis.com/token',
+  auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL
+};
+
+if (!serviceAccount.private_key || !serviceAccount.project_id || !serviceAccount.client_email || !serviceAccount.client_id) {
+  throw new Error('Missing necessary Firebase environment variables');
+}
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
+  databaseURL: process.env.FIREBASE_DATABASE_URL
 });
 
 const db = admin.firestore();
